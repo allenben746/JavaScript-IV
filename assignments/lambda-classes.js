38774be7ae0c2a15d35629f0fdb68b118a47d637
Ben <allenben@protonmail.com>
2019-06-13 03:34:06 -0500

    Created Instructor Class v.2

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,15 +1,30 @@
 // CODE here for your Lambda Classes
-class person {
+class person { //This is the Class that all other classes will extend from - Ben Allen
     constructor(name, age, location){
         this.name = name;
         this.age = age;
         this.location = location;
     }
-    speak () {
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    speak() { //Method
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
     }
 }
 
+class Insructor extends person {
+    constructor(name, age, location, specialty, favLanguage, catchPhrase){
+        super(name, age, location);
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase = catchPhrase;
+    }
+    demo(subject) { //Method
+        console.log(`Today we are learning about ${subject}`);
+    }
+    
+    grade(studentObj, subject) { //Method
+        console.log(`${studentObj.name} receives a perfect score on ${subject}`);
+    }
+    
+}
 const Ben = new person("Ben", "21", "Houston");
-
 Ben.speak();
