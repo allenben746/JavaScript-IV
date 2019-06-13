fdeecef0aaec9b2faadedca06c95f0a55232a3c1
Ben <allenben@protonmail.com>
2019-06-13 03:17:18 -0500

    Created parent class person in lambda-classes.js v 0.1

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,15 @@
 // CODE here for your Lambda Classes
+class person {
+    constructor(name, age, location){
+        this.name = name;
+        this.age = age;
+        this.location = location;
+    }
+    speak () {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+const Ben = new person("Ben", "21", "Houston");
+
+Ben.speak();
